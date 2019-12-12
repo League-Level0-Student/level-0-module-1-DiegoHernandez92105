48da96677d424fa558a453fdb17229df095792e1
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-12-12 13:23:24 -0800

    my ages and badgers

---
+++ src/_11_my_ages/my_ages.java
@@ -0,0 +1,17 @@
+package _11_my_ages;
+
+public class my_ages {
+
+	public static void main(String[] args) {
+		for (int age = 0; age<13; age++ ) {
+			
+			System.out.println(age);
+		
+
+	if(age == 12 ) {
+	System.out.println("that is all the years that i have been alive for");
+	
+		}	
+	}
+	}
+}
---
+++ src/_12_badgers/Badgers.java
@@ -0,0 +1,32 @@
+
+package _12_badgers;
+import java.util.Random;
+public class Badgers {
+
+	public static void main(String[] args) {
+		Random factory = new Random();
+		System.out.print("Hey guys here is my new song I have been working on.");
+		System.out.println("");
+		for (int repeat = 0; repeat<100; repeat++) {
+			int random = factory.nextInt(6);
+			
+			if (random == 0) {
+						
+				System.out.print(" Badger");
+				
+			}
+			else if (random == 1) {
+				System.out.print(" snake ");
+			}
+			else if (random == 2) {
+				System.out.print(" mushroom ");
+			}
+				else if (random == 5) {
+					System.out.println("");
+				}
+			
+		}
+
+	}
+
+}
